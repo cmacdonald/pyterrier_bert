c1ac1007ec7a9d46bde3f5db80643aaecde324ff
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2020-05-09 10:33:40 +0100

    fix egg names

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ with open('requirements.txt', 'rt') as f:
     for req in f.read().splitlines():
         if req.startswith('git+'):
             pkg_name = req.split('/')[-1].replace('.git', '')
+            if "#egg=" in pkg_name:
+                pkg_name = pkg_name.split("#egg=")[1]
             requirements.append(f'{pkg_name} @ {req}')
         else:
             requirements.append(req)
-
+#print(requirements)
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
