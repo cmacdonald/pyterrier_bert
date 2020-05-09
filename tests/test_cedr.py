8c2cc85ca28e49fbdcdfd57f62633b0c919a0647
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2020-05-09 10:17:22 +0100

    small test fixes

--- tests/test_cedr.py
+++ tests/test_cedr.py
@@ -8,14 +8,23 @@ from pyterrier_bert.pyt_cedr import CEDRPipeline
 
 class TestCEDR(unittest.TestCase):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not pt.started():
+            pt.init()
+
     def test_fit(self):
+        qrels = pd.DataFrame([
+            ["q1", "d1", 1],
+            ["q1", "d2", 0],
+        ], columns=["qid", "docno", "label"])
         df = pd.DataFrame([
-            ["q1", "query text", "d1", "doc text", 1],
-            ["q1", "query text", "d1", "this is an irrelevant document", 0],
-            ], columns=["qid", "query", "docno", "text", "label"])
+            ["q1", "query text", "d1", "doc text"],
+            ["q1", "query text", "d2", "this is an irrelevant document"],
+            ], columns=["qid", "query", "docno", "text"])
 
         pipe = CEDRPipeline(doc_attr="text")
-        pipe.fit(df, None, df, None)
+        pipe.fit(df, qrels, df, qrels)
         rtr = pipe.transform(df)
         self.assertEqual(2, len(rtr))
         self.assertTrue("score" in rtr.columns)
