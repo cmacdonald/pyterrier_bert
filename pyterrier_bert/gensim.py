0cfa093be72d223cf28d3988cb55c4d8717e171c
Craig Macdonald <craig.macdonald@glasgow.ac.uk>
2020-07-01 21:52:33 +0100

    add average cosine

--- pyterrier_bert/gensim.py
+++ pyterrier_bert/gensim.py
@@ -2,32 +2,71 @@ import gensim.downloader as api
 from tqdm import tqdm
 from pyterrier.transformer import TransformerBase
 from gensim.utils import tokenize
+import numpy as np
+from numpy import dot
+from numpy.linalg import norm
 
-class GensimWordMoverDistance(TransformerBase):
+class GensimBase(TransformerBase):
+
+  def __init__(self, doc_attr="body", modelname="glove-wiki-gigaword-100", verbose=0):
+    self.doc_attr = doc_attr
+    self.wv = api.load(modelname)
+    self.verbose = verbose
+    self.oov = np.random.rand(self.wv.vector_size)
+
+  def transform(self, topics_res):
+    pass
+
+
+class GensimAverageSimilarity(GensimBase):
+  '''
+    A similarity score that computes the cosine similarity on the 
+    average of the document and query term embedding vectors
+  '''
+  def transform(self, topics_res):
+
+    def lambda_row(row):
+      q = list(tokenize(row["query"]))
+      d = list(tokenize(row[self.doc_attr]))
+
+      qs = np.array([ self.wv[t] if t in self.wv else self.oov for t in q ])
+      ds = np.array([ self.wv[t] if t in self.wv else self.oov for t in d ])
+      qs_avg = np.average(qs, axis=0)
+      ds_avg = np.average(ds, axis=0)   
+
+      return qs_avg @ qs_avg.T/(norm(qs_avg)*norm(ds_avg)) 
+
+    # could take a while, add a progress bar if asked to
+    if self.verbose:
+      tqdm.pandas()
+      topics_res["score"] = topics_res.progress_apply(lambda_row, axis=1)
+    else:
+      topics_res["score"] = topics_res.apply(lambda_row, axis=1)
+
+    return topics_res
     
-    def __init__(self, doc_attr="body", verbose=0):
-        self.doc_attr = doc_attr
-        self.wv = api.load("glove-wiki-gigaword-100")
-        self.verbose = verbose
 
-        # When using the wmdistance method, it is beneficial to normalize 
-        # the word2vec vectors first, so they all have equal length.
-        self.wv.init_sims(replace=True)
-        
+class GensimWordMoverDistance(GensimBase):
+  '''
+    A similarity score that uses WordMoversDistance
+  '''
+  
+  def __init__(self, **kwargs):
+    super().__init__(**kwargs)
+    # When using the wmdistance method, it is beneficial to normalize 
+    # the word2vec vectors first, so they all have equal length.
+    self.wv.init_sims(replace=True)
 
-    # def preprocess(self, sentence):
-    #     return [w for w in tokenize(sentence) if w not in self.stop_words]
+  def transform(self, topics_res):
+    # wmdistance is a _distance_, so we take the negative as our "similarity" score
 
-    def transform(self, topics_res):
-        # wmdistance is a _distance_, so we take the negative as our score
+    lambda_row = lambda row: self.wv.wmdistance(list(tokenize(row["query"])), list(tokenize(row[self.doc_attr])))
 
-        lambda_row = lambda row: self.wv.wmdistance(list(tokenize(row["query"])), list(tokenize(row[self.doc_attr])))
+    # could take a while, add a progress bar if asked to
+    if self.verbose:
+      tqdm.pandas()
+      topics_res["score"] = -1 * topics_res.progress_apply(lambda_row, axis=1)
+    else:
+      topics_res["score"] = -1 * topics_res.apply(lambda_row, axis=1)
 
-        # could take a while, add a progress bar if asked to
-        if self.verbose:
-          tqdm.pandas()
-          topics_res["score"] = -1 * topics_res.progress_apply(lambda_row, axis=1)
-        else:
-          topics_res["score"] = -1 * topics_res.apply(lambda_row, axis=1)
-
-        return topics_res
+    return topics_res
--- tests/test_gensim.py
+++ tests/test_gensim.py
@@ -1,11 +1,20 @@
 import pandas as pd
 import pyterrier_bert
-from pyterrier_bert.gensim import GensimWordMoverDistance
+from pyterrier_bert.gensim import GensimWordMoverDistance, GensimAverageSimilarity
 import unittest
 class TestGensim(unittest.TestCase):
 
+    def test_gensim_cos(self):
+        df = pd.DataFrame([
+            ["q1", 'Obama speaks to the media in Illinois', "d1", 'The president greets the press in Chicago'],
+            ["q1", 'Obama speaks to the media in Illinois', "d2", 'Holiday accommodation owners have been deluged with bookings over the last 48 hours.']]
+            , columns=["qid", "query", "docno", "body"])
+        pipe = GensimAverageSimilarity()
+        rtr = pipe(df)
+        print(rtr)
+        self.assertTrue(df["score"][0] > df["score"][1])
 
-    def test_gensim(self):
+    def test_gensim_wmd(self):
         df = pd.DataFrame([
             ["q1", 'Obama speaks to the media in Illinois', "d1", 'The president greets the press in Chicago'],
             ["q1", 'Obama speaks to the media in Illinois', "d2", 'Holiday accommodation owners have been deluged with bookings over the last 48 hours.']]
